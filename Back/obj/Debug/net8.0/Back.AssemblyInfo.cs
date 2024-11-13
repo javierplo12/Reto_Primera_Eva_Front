c624bd5aa315939b31900e60e45fd2bc739fce9f
Ruben Arnadillo <a25612@svalero.com>
2024-11-13 17:12:40 +0100

    Añadimos Todas las peliculas

--- Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
+++ Back/obj/Debug/net8.0/Back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238125df8452b3f1ee590010e36be85fa9bc4b5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc8e9ab2555a5d2a41a506855bf11ab5b1d27dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
