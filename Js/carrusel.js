1b1afc7b93678096841527c48e220403bd43e278
a25612 <a25612@svalero.com>
2024-12-16 13:16:15 +0100

    actualizamos las rutas de las peliculas del carrusel

--- Js/carrusel.js
+++ Js/carrusel.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Obtener los elementos necesarios
     const track = document.querySelector('.carousel__track');
     const slides = Array.from(track.children);
     const nextButton = document.querySelector('.carousel__control--next');
@@ -9,25 +8,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Función para mover el carrusel
     const moveToSlide = (index) => {
-        const slideWidth = slides[0].getBoundingClientRect().width; // Obtener el ancho de una imagen
+        const slideWidth = slides[0].getBoundingClientRect().width;
         console.log(`Moving to slide ${index}`);
-        track.style.transform = `translateX(-${index * slideWidth}px)`; // Desplazar el track
+        track.style.transform = `translateX(-${index * slideWidth}px)`;
         currentIndex = index; // Actualizar el índice
     };
 
     // Función para pasar a la siguiente imagen
     const nextSlide = () => {
-        const nextIndex = (currentIndex + 1) % slides.length; // Avanzar al siguiente slide
+        const nextIndex = (currentIndex + 1) % slides.length; 
         moveToSlide(nextIndex);
     };
 
     // Función para retroceder a la imagen anterior
     const prevSlide = () => {
-        const prevIndex = (currentIndex - 1 + slides.length) % slides.length; // Retroceder al slide anterior
+        const prevIndex = (currentIndex - 1 + slides.length) % slides.length;
         moveToSlide(prevIndex);
     };
 
-    // Agregar eventos a los botones "Siguiente" y "Anterior"
+    // Agregar funciones a los botones Siguiente y Anterior
     nextButton.addEventListener('click', nextSlide);
     prevButton.addEventListener('click', prevSlide);
 });
